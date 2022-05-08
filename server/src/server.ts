da4fedafb61a099bf24559263917818bebe14456
Eduardo Reichert <edrdesigner@gmail.com>
2022-05-08 18:00:52 -0300

    fix port for railway

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,6 +8,7 @@ app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(routes);
 
-app.listen(3333, () => {
+app.listen(process.env.PORT || 3333, () => {
   console.log('HTTP server running! on port 3333');
 });
+ 
